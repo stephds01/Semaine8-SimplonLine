545f89d346768f7c42da7b3919bcb785fb234ebb
stephds01 <stephds01@gmail.com>
2015-11-13 22:17:06 +0100

    Exercice sur formulaire

---
+++ Portfolio-bootstrap/traitement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head >
+    <meta charset="UTF-8">
+    <title>Portfolio Bootstrap</title>
+    <meta charset="utf-8">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div class="container">
+    <h1 text-center>Votre message a bien été envoyé !</h1>
+    </div>
+
+</body>
+
+</html>
